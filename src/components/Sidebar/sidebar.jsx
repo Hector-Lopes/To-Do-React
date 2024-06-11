512d57896de7dc35a54dc674452dbf59966cd98b
hector.lopes <hector.lopes@anchieta.br>
2024-06-11 15:03:15 -0300

    feat: add signout button

--- src/components/Sidebar/sidebar.jsx
+++ src/components/Sidebar/sidebar.jsx
@@ -1,18 +1,30 @@
 import Button from "../button/button";
 
+import { useNavigate } from "react-router-dom";
+
 import logo from "../../assets/images/logo.png";
 import "./sidebar.scss";
+
 const Sidebar = () => {
-  return (
-    <>
-      <div className="sidebar-container">
-        <div className="logo">
-          <img src={logo} alt="Full Stack" />
-        </div>
-        <Button>Sair</Button>
-      </div>
-    </>
-  );
+	const navigate = useNavigate();
+
+	const HandleClickinNavigate = () => {
+		navigate("/login");
+	};
+
+	return (
+		<>
+			<div className='sidebar-container'>
+				<div className='logo'>
+					<img
+						src={logo}
+						alt='Full Stack'
+					/>
+				</div>
+				<Button onClick={HandleClickinNavigate}>Sair</Button>
+			</div>
+		</>
+	);
 };
 
 export default Sidebar;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,15 +2,21 @@ import Button from "../components/button/button";
 
 import "./Login.scss";
 import logo from "../assets/images/logo.png";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+	const navigate = useNavigate();
+
+	const handleSignInClick = () => {
+		navigate("/");
+	};
 	return (
 		<div className='login-container'>
 			<img
 				src={logo}
 				alt='full estack club'></img>
 			<div className='custom-buuton-container'></div>
-			<Button>Entrar</Button>
+			<Button onClick={handleSignInClick}>Entrar</Button>
 		</div>
 	);
 };
