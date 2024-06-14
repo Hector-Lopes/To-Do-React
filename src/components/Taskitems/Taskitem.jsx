1262f01620db2fbd1fcc93d0fdeecf4761a50288
hector.lopes <hector.lopes@anchieta.br>
2024-06-14 14:34:23 -0300

    test

--- src/components/Taskitems/Taskitem.jsx
+++ src/components/Taskitems/Taskitem.jsx
@@ -1,72 +1,72 @@
-import "./Taskitem.scss";
+import './Taskitem.scss'
 
-import { AiFillDelete } from "react-icons/ai";
-import axios from "axios";
+import { AiFillDelete } from 'react-icons/ai'
+import axios from "axios"
 
 const TaskItem = ({ task, fetchTasks }) => {
-	const HandleTaskDeletion = async () => {
-		try {
-			await axios.delete(
-				`https://fsc-task-manager-backend.herokuapp.com/tasks/${task._id}`,
-			);
-			fetchTasks();
+  const HandleTaskDeletion = async () => {
+    try {
+      await axios.delete(
+        `https://fsc-task-manager-backend.herokuapp.com/tasks/${task._id}`
+      )
+      fetchTasks()
 
-			alert("A Tarefa foi Removida com sucesso");
-		} catch (_error) {
-			alert("Algo deu errado.");
-		}
-	};
+      alert('A Tarefa foi Removida com sucesso')
+    } catch (_error) {
+      alert('Algo deu errado.')
+    }
+  }
 
-	const handleTaskCompletecheck = async (e) => {
-		try {
-			await axios.patch(
-				`https://fsc-task-manager-backend.herokuapp.com/tasks/${task._id}`,
-				{
-					isCompleted: e.target.checked,
-				},
-			);
-			alert.success("A Tarefa foi Concluida");
-			await fetchTasks();
-		} catch (_error) {
-			alert("Algo deu errado");
-		}
-	};
+  const handleTaskCompletecheck = async (e) => {
+    try {
+      await axios.patch(
+        `https://fsc-task-manager-backend.herokuapp.com/tasks/${task._id}`,
+        {
+          isCompleted: e.target.checked
+        }
+      )
+      alert.success('A Tarefa foi Concluida')
+      await fetchTasks()
+    } catch (_error) {
+      alert('Algo deu errado')
+    }
+  }
 
-	return (
-		<>
-			<div className='task-item-container'>
-				<div className='task-description'>
-					<label
-						className={
-							task.isCompleted
-								? "checkbox-container-completed"
-								: "checkbox-container"
-						}>
-						{task.description}
-						<input
-							type='checkbox'
-							defaultChecked={task.isCompleted}
-							onChange={(e) => handleTaskCompletecheck(e)}
-						/>
-						<span
-							className={
-								task.isCompleted ? "checkmark completed" : "checkmark"
-							}></span>
-					</label>
-				</div>
-				<div className='delete'>
-					<AiFillDelete
-						size={20}
-						color='#f97474'
-						onClick={HandleTaskDeletion}
-					/>
-				</div>
-			</div>
-		</>
-	);
-};
+  return (
+    <>
+      <div className='task-item-container'>
+        <div className='task-description'>
+          <label
+            className={
+              task.isCompleted
+                ? 'checkbox-container-completed'
+                : 'checkbox-container'
+            }
+          >
+            {task.description}
+            <input
+              type='checkbox'
+              defaultChecked={task.isCompleted}
+              onChange={(e) => handleTaskCompletecheck(e)}
+            />
+            <span
+              className={task.isCompleted ? 'checkmark completed' : 'checkmark'}
+            ></span>
+          </label>
+        </div>
+        <div className='delete'>
+          <AiFillDelete
+            size={20}
+            color='#f97474'
+            onClick={HandleTaskDeletion}
+          />
+        </div>
+      </div>
+    </>
+  )
+}
 
-export default TaskItem;
+export default TaskItem
 
 // import React from "react";
 
