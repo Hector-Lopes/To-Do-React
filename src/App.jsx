cbd946fb03195012aa6d69ca9fda4390fc72249b
hector.lopes <hector.lopes@anchieta.br>
2024-06-14 11:19:47 -0300

    feat: add eslint e prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,15 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route } from 'react-router-dom'
 
-import "./App.scss";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
+import './App.scss'
+import Home from './pages/Home'
+import Login from './pages/Login'
 const App = () => {
-	return (
-		<Routes>
-			<Route
-				path='/:name'
-				element={<Home />}></Route>
-			<Route
-				path='/'
-				element={<Login />}></Route>
-		</Routes>
-	);
-};
+  return (
+    <Routes>
+      <Route path='/:name' element={<Home />}></Route>
+      <Route path='/' element={<Login />}></Route>
+    </Routes>
+  )
+}
 
-export default App;
+export default App
