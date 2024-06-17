6c1c6d5faec9da6372500110e637b9e57090b15f
hector.lopes <hector.lopes@anchieta.br>
2024-06-17 08:59:55 -0300

    feat: add  git-commit-msg-linter

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { Routes, Route } from 'react-router-dom'
 
 import './App.scss'
-import Home from "./pages/Home"
+import Home from './pages/Home'
 import Login from './pages/Login'
 const App = () => {
   return (
     <Routes>
-      <Route path="/:name" element={<Home />}></Route>
+      <Route path='/:name' element={<Home />}></Route>
       <Route path='/' element={<Login />}></Route>
     </Routes>
   )
