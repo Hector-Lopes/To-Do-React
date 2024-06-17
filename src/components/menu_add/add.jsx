593b0929084851e903c33451c98683c800a6ef18
hector.lopes <hector.lopes@anchieta.br>
2024-06-17 09:22:17 -0300

    feat: change url to env variables

--- src/components/menu_add/add.jsx
+++ src/components/menu_add/add.jsx
@@ -1,53 +1,53 @@
-import { FaPlus } from "react-icons/fa";
+import { FaPlus } from 'react-icons/fa'
 
-import "./add.scss";
+import './add.scss'
 
-import { useState } from "react";
-import axios from "axios";
+import { useState } from 'react'
+import axios from 'axios'
 
-import Button from "../button/button";
-import Input from "../Input/input";
+import Button from '../button/button'
+import Input from '../Input/input'
 
 const ADD = ({ fetchTasks }) => {
-	const [task, SetTask] = useState("");
+  const [task, SetTask] = useState('')
 
-	const onChangee = (e) => {
-		SetTask(e.target.value);
-	};
+  const onChangee = (e) => {
+    SetTask(e.target.value)
+  }
 
-	const HandleTaskAddition = async () => {
-		try {
-			if (task.length === 0) {
-				return alert("A Tarefa precisa de uma descrição");
-			}
-			await axios.post("https://fsc-task-manager-backend.herokuapp.com/tasks", {
-				description: task,
-				isCompleted: false,
-			});
+  const HandleTaskAddition = async () => {
+    try {
+      if (task.length === 0) {
+        return alert('A Tarefa precisa de uma descrição')
+      }
+      await axios.post(`${process.env.REACT_APP_API_URL}/tasks`, {
+        description: task,
+        isCompleted: false
+      })
 
-			await fetchTasks();
-			SetTask("");
-			await alert("A Tarefa foi adicionada com sucesso!");
-		} catch (_error) {
-			alert("Algo deu errado");
-		}
-	};
+      await fetchTasks()
+      SetTask('')
+      await alert('A Tarefa foi adicionada com sucesso!')
+    } catch (_error) {
+      alert('Algo deu errado')
+    }
+  }
 
-	return (
-		<>
-			<div className='add-task-container'>
-				<Input
-					label='Adicionar tarefa'
-					value={task}
-					onChange={onChangee}
-					onEnterPress={HandleTaskAddition}
-				/>
-				<Button onClick={HandleTaskAddition}>
-					<FaPlus>color="#ffff"</FaPlus>
-				</Button>
-			</div>
-		</>
-	);
-};
+  return (
+    <>
+      <div className='add-task-container'>
+        <Input
+          label='Adicionar tarefa'
+          value={task}
+          onChange={onChangee}
+          onEnterPress={HandleTaskAddition}
+        />
+        <Button onClick={HandleTaskAddition}>
+          <FaPlus>color="#ffff"</FaPlus>
+        </Button>
+      </div>
+    </>
+  )
+}
 
-export default ADD;
+export default ADD
